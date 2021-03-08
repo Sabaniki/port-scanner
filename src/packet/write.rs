941f939a3f528de75cde4b876531dece30cd3a9d
Sabaniki <sawada13@mccc.jp>
2021-03-08 22:53:07 +0900

    write.rsを追加し、build_packetを実装

---
+++ src/packet/write.rs
@@ -0,0 +1,26 @@
+use crate::packet::data::PacketInfo;
+use pnet::packet::tcp::{self, MutableTcpPacket, TcpFlags};
+const TCP_SIZE:usize = 20;
+
+// パケットを作成する
+pub fn build_packet(packet_info: &PacketInfo) -> [u8; TCP_SIZE] {
+    // 0u8: 符号なし8bit整数としての0
+    let mut tcp_buffer = [0u8; TCP_SIZE];
+    let mut tcp_header = MutableTcppacket::new(&mut tcp_buffer[..]).unwrap();
+    tcp_header.set_source(packet_info.my_port);
+
+    // オプションを含まないため20オクテットまでがTCPヘッダ。
+    // 4オクテット単位で指定するため引数は5。
+    // オプションやコントロールフラグについて忘れたら
+    // https://www.infraexpert.com/study/tcpip8.html を参照
+    tcp_header.set_data_offset(5);
+    tcp_header.set_flags(packet_info.scan_type as u16);
+    let checksum = tcp::ipv4_checksum(
+        &tcp_header.to_immutable(),
+        &packet_info.my_ip_address,
+        &packet_info.target_ip_address,
+    );
+    tcp_header.set_checksum(checksum);
+
+    tcp_buffer
+}
